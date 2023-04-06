f13e725a71b1798ad34fbb74d834b664372aef5f
Cedric Skwar <cdrc@skwar.me>
2023-04-06 11:39:11 +0200

    Implemented a sign and calculate function

--- encryption.go
+++ encryption.go
@@ -5,6 +5,9 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"fmt"
+	"math/big"
+
+	"golang.org/x/crypto/blake2b"
 )
 
 func generateECCKeyPair(curve elliptic.Curve) (*ecdsa.PrivateKey, *ecdsa.PublicKey, error) {
@@ -18,6 +21,45 @@ func generateECCKeyPair(curve elliptic.Curve) (*ecdsa.PrivateKey, *ecdsa.PublicK
 	return privateKey, publicKey, nil
 }
 
+func calculateHash(message []byte) []byte {
+	hash, err := blake2b.New256(nil)
+	if err != nil {
+		fmt.Printf("Error creating hash: %v\n", err)
+		return nil
+	}
+	hash.Write(message)
+	return hash.Sum(nil)
+}
+
+func sign(privateKey *ecdsa.PrivateKey, messageHash []byte) ([]byte, error) {
+	r, s, err := ecdsa.Sign(rand.Reader, privateKey, messageHash)
+	if err != nil {
+		return nil, err
+	}
+
+	curveBits := privateKey.PublicKey.Curve.Params().BitSize
+	keyBytes := (curveBits + 7) / 8
+
+	signature := make([]byte, keyBytes*2)
+	rBytes := r.Bytes()
+	sBytes := s.Bytes()
+
+	copy(signature[keyBytes-len(rBytes):], rBytes)
+	copy(signature[keyBytes*2-len(sBytes):], sBytes)
+
+	return signature, nil
+}
+
+func verify(publicKey *ecdsa.PublicKey, messageHash []byte, signature []byte) bool {
+	curveBits := publicKey.Curve.Params().BitSize
+	keyBytes := (curveBits + 7) / 8
+
+	r := new(big.Int).SetBytes(signature[:keyBytes])
+	s := new(big.Int).SetBytes(signature[keyBytes:])
+
+	return ecdsa.Verify(publicKey, messageHash, r, s)
+}
+
 func main() {
 	curve := elliptic.P256()
 	privateKey, publicKey, err := generateECCKeyPair(curve)
@@ -26,7 +68,22 @@ func main() {
 		return
 	}
 
-	fmt.Printf("Private Key: %x\n", privateKey.D.Bytes())
-	fmt.Printf("Public Key (X): %x\n", publicKey.X.Bytes())
-	fmt.Printf("Public Key (Y): %x\n", publicKey.Y.Bytes())
+	msg := []byte("Hello World!")
+
+	hash := calculateHash(msg)
+	fmt.Printf("Hash: %x\n", hash)
+	signature, err := sign(privateKey, hash)
+	if err != nil {
+		fmt.Printf("Error signing message: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Signature: %x\n", signature)
+
+	valid := verify(publicKey, hash, signature)
+	if valid {
+		fmt.Println("Signature is valid!")
+	} else {
+		fmt.Println("Signature is not valid!")
+	}
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module cedric.skwar.me/encryption
 go 1.20
 
 require golang.org/x/crypto v0.7.0
+
+require golang.org/x/sys v0.6.0 // indirect
