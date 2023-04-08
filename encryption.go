8c0eab56d19d27c4c8d0fe1c807d79a56d4c6bcd
Cédric Skwar <cdrc@skwar.me>
2023-04-08 11:54:25 +0200

    Added the cloudfalre dilithium

--- encryption.go
+++ encryption.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"math/big"
 
+	"github.com/cloudflare/circl/sign/dilithium"
 	"golang.org/x/crypto/blake2b"
 )
 
@@ -31,7 +32,7 @@ func calculateHash(message []byte) []byte {
 	return hash.Sum(nil)
 }
 
-func sign(privateKey *ecdsa.PrivateKey, messageHash []byte) ([]byte, error) {
+func signEcc(privateKey *ecdsa.PrivateKey, messageHash []byte) ([]byte, error) {
 	r, s, err := ecdsa.Sign(rand.Reader, privateKey, messageHash)
 	if err != nil {
 		return nil, err
@@ -50,7 +51,7 @@ func sign(privateKey *ecdsa.PrivateKey, messageHash []byte) ([]byte, error) {
 	return signature, nil
 }
 
-func verify(publicKey *ecdsa.PublicKey, messageHash []byte, signature []byte) bool {
+func verifyEcc(publicKey *ecdsa.PublicKey, messageHash []byte, signature []byte) bool {
 	curveBits := publicKey.Curve.Params().BitSize
 	keyBytes := (curveBits + 7) / 8
 
@@ -60,9 +61,63 @@ func verify(publicKey *ecdsa.PublicKey, messageHash []byte, signature []byte) bo
 	return ecdsa.Verify(publicKey, messageHash, r, s)
 }
 
+func generateDilithiumKeyPair(modeName string) (dilithium.PublicKey, dilithium.PrivateKey, error) {
+	mode := dilithium.ModeByName(modeName)
+	if mode == nil {
+		return nil, nil, fmt.Errorf("mode not supported")
+	}
+
+	publicKey, privateKey, err := mode.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error generating key pair: %v", err)
+	}
+
+	return publicKey, privateKey, nil
+}
+
+func packDilithiumKeys(
+	publicKey dilithium.PublicKey,
+	privateKey dilithium.PrivateKey,
+) ([]byte, []byte) {
+	return publicKey.Bytes(), privateKey.Bytes()
+}
+
+func unpackDilithiumKeys(
+	modeName string,
+	packedPublicKey []byte,
+	packedPrivateKey []byte,
+) (dilithium.PublicKey, dilithium.PrivateKey) {
+	mode := dilithium.ModeByName(modeName)
+
+	return mode.PublicKeyFromBytes(packedPublicKey), mode.PrivateKeyFromBytes(packedPrivateKey)
+}
+
+func signDilithium(privateKey dilithium.PrivateKey, msg []byte, modeName string) ([]byte, error) {
+	mode := dilithium.ModeByName(modeName)
+	if mode == nil {
+		return nil, fmt.Errorf("mode not supported")
+	}
+
+	return mode.Sign(privateKey, msg), nil
+}
+
+func verifyDilithium(
+	publicKey dilithium.PublicKey,
+	msg []byte,
+	signature []byte,
+	modeName string,
+) (bool, error) {
+	mode := dilithium.ModeByName(modeName)
+	if mode == nil {
+		return false, fmt.Errorf("mode not supported")
+	}
+
+	return mode.Verify(publicKey, msg, signature), nil
+}
+
 func main() {
 	curve := elliptic.P256()
-	privateKey, publicKey, err := generateECCKeyPair(curve)
+	privateKeyEcc, publicKeyEcc, err := generateECCKeyPair(curve)
 	if err != nil {
 		fmt.Printf("Error generating key pair: %v\n", err)
 		return
@@ -71,16 +126,50 @@ func main() {
 	msg := []byte("Profil. Berufsvorbereitung")
 
 	hash := calculateHash(msg)
-	signature, err := sign(privateKey, hash)
+	signature, err := signEcc(privateKeyEcc, hash)
 	if err != nil {
 		fmt.Printf("Error signing message: %v\n", err)
 		return
 	}
 
-	valid := verify(publicKey, hash, signature)
+	valid := verifyEcc(publicKeyEcc, hash, signature)
 	if valid {
 		fmt.Println("Signature is valid!")
 	} else {
 		fmt.Println("Signature is not valid!")
+	}
+
+	modeName := "Dilithium5-AES"
+	// Generate Dilithium key pair
+	publicKey, privateKey, err := generateDilithiumKeyPair(modeName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Pack and unpack Dilithium keys
+	packedPublicKey, packedPrivateKey := packDilithiumKeys(publicKey, privateKey)
+	publicKey2, privateKey2 := unpackDilithiumKeys(modeName, packedPublicKey, packedPrivateKey)
+
+	// Sign and verify with Dilithium keys
+	signature, err = signDilithium(privateKey2, msg, modeName)
+	if err != nil {
+		fmt.Println(
+			"ERROR: The error occurred while signing the message with CRYSTAl-Dilithium. The error is: ",
+			err,
+		)
+	}
+	valid, err = verifyDilithium(publicKey2, msg, signature, modeName)
+	if err != nil {
+		fmt.Println(
+			"ERROR: The error occurred while validating the message with CRYSTAl-Dilithium. The error is: ",
+			err,
+		)
+	}
+
+	if valid {
+		fmt.Println("CRYSTAl-Dilithium Signature is valid!")
+	} else {
+		fmt.Println("CRYSTAl-Dilithium Signature is not valid!")
 	}
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.20
 
 require golang.org/x/crypto v0.7.0
 
-require golang.org/x/sys v0.6.0 // indirect
+require (
+	github.com/cloudflare/circl v1.3.2 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
