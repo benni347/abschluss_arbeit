d4cae2b02e3d78cd52368e007bb046bef5e3f6d8
Cédric Skwar <cdrc@skwar.me>
2023-05-01 11:45:55 +0200

    started implementing my webrtc

--- messenger/app.go
+++ messenger/app.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+
+	utils "github.com/benni347/messengerutils"
+	webrtc "github.com/pion/webrtc/v3"
 )
 
 // App struct
@@ -18,4 +21,44 @@ func NewApp() *App {
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
+}
+
+func (a *App) CreatePeerConnection() *webrtc.PeerConnection {
+	// Create a new RTCPeerConnection
+	peerConnection, err := webrtc.NewPeerConnection(webrtc.Configuration{})
+	if err != nil {
+		utils.PrintError(err)
+		panic(err)
+	}
+
+	return peerConnection
+}
+
+func (a *App) TransmitDataText(peerConnection *webrtc.PeerConnection, data string) {
+	protcol := "tcp"
+	order := true
+	var maxReTransmission uint16 = 5000
+
+	dataChannelInit := webrtc.DataChannelInit{
+		Ordered:        &order,
+		MaxRetransmits: &maxReTransmission,
+		Protocol:       &protcol,
+	}
+	// Create a datachannel with label 'data'
+	dataChannel, err := peerConnection.CreateDataChannel("text", &dataChannelInit)
+	if err != nil {
+		utils.PrintError("During the creation of the data channel an error ocured", err)
+		panic(err)
+	}
+
+	// Register channel opening handling
+	dataChannel.OnOpen(func() {
+		// Send the data
+		dataChannel.SendText(data)
+	})
+
+	// Register text message handling
+	dataChannel.OnMessage(func(msg webrtc.DataChannelMessage) {
+		println("Received message: " + string(msg.Data))
+	})
 }
