2ef6beba17c45e485b9e6636ae38e87e2c51646d
Cédric Skwar <cdrc@skwar.me>
2023-05-03 15:01:39 +0200

    ViewManager interface.

---
+++ examples/gio/alarm/view_manager.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"runtime"
+	"time"
+
+	"gioui.org/app"
+	"gioui.org/f32"
+	"gioui.org/io/profile"
+	"gioui.org/layout"
+	"gioui.org/op/clip"
+	"gioui.org/op/paint"
+	"gioui.org/unit"
+	"gioui.org/widget/material"
+	"gioui.org/x/component"
+	"git.sr.ht/~whereswaldon/sprig/core"
+	"git.sr.ht/~whereswaldon/sprig/icons"
+	sprigTheme "git.sr.ht/~whereswaldon/sprig/widget/theme"
+)
+
+type ViewManager interface {
+	RequestViewSwitch(viewId)
+	SetView(viewId)
+	RegisterView(id viewId, view View)
+	RegisterIntentHandler(intent Intent) bool
+	RequestInvalidate()
+	HandleBackNavigation()
+	RequestContextualBar(
+		gtx layout.Context,
+		title string,
+		actions []component.AppBarAction,
+		overflow []component.OverflowAction,
+	)
+	DismissContextualBar(gtx layout.Context)
+	DismissOverflow(gtx layout.Context)
+	SelectedOverflowTag() interface{}
+	Layout(gtx layout.Context) layout.Dimensions
+	SetProfiling(bool)
+	SetThemeing(bool)
+	ApplySettings(core.SettingsService)
+}
